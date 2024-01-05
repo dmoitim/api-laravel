ad623596230596867e502a48f2f99a7910c7253d
Devair Luiz Moitim <dmoitim@empro.com.br>
2024-01-05 17:21:59 -0300

    Inclui a atualização de invoice e melhora validação na inserção

--- app/Http/Controllers/Api/v1/InvoiceController.php
+++ app/Http/Controllers/Api/v1/InvoiceController.php
@@ -29,14 +29,14 @@ class InvoiceController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'user_id' => 'required',
-            'type' => 'required|max:1',
+            'type' => 'required|max:1|in:' . implode(',', ['B', 'C', 'P']),
             'paid' => 'required|numeric|between:0,1',
-            'payment_date' => 'nullable',
+            'payment_date' => 'nullable|date_format:Y-m-d H:i:s',
             'value' => 'required|numeric|between:0.01,9999.99'
         ]);
 
         if ($validator->fails()) {
-            return $this->error('Ocorreu um erro ao tentar processar a chamada.', 422, $validator->errors());
+            return $this->error('Ocorreu um erro ao tentar processar a inclusão.', 422, $validator->errors());
         }
 
         $created = Invoice::create($validator->validated());
@@ -59,9 +59,35 @@ class InvoiceController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Invoice $invoice)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required',
+            'type' => 'required|max:1|in:' . implode(',', ['B', 'C', 'P']),
+            'paid' => 'required|numeric|between:0,1',
+            'payment_date' => 'nullable|date_format:Y-m-d H:i:s',
+            'value' => 'required|numeric|between:0.01,9999.99'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->error('Ocorreu um erro ao tentar processar a alteração.', 422, $validator->errors());
+        }
+
+        $validated = $validator->validated();
+
+        $updated = $invoice->update([
+            'user_id' => $validated['user_id'],
+            'type' => $validated['type'],
+            'paid' => $validated['paid'],
+            'payment_date' => $validated['paid'] ? $validated['payment_date'] : NULL,
+            'value' => $validated['value']
+        ]);
+
+        if (!$updated) {
+            return $this->error('Invoice não atualizado. Ocorreu um erro ao tentar gravar no banco de dados.', 400);
+        }
+
+        return $this->success('Invoice atualizado com sucesso.', 200, new InvoiceResource($invoice->load('user')));
     }
 
     /**
